4f7fa69d5a62393a83ef9a26916328df4779c23f
karol <pamela.loza199@gmail.com>
2018-05-09 18:24:51 +0200

    ahora va Pamela

--- app/src/main/java/fitness/sportgenertaion/fitnesstrainer/CrearRutina.java
+++ app/src/main/java/fitness/sportgenertaion/fitnesstrainer/CrearRutina.java
@@ -1,27 +1,23 @@
 package fitness.sportgenertaion.fitnesstrainer;
 
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -119,6 +115,7 @@ public class CrearRutina extends AppCompatActivity implements AdapterView.OnItem
 
 
         //Hago una conversion
+        //4
 
 
     }
