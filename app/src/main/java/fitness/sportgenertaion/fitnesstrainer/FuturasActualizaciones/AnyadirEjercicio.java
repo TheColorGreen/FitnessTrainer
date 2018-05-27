d056712285e3e5303b3dc11c5a0b216bb1e1064b
Carlos Zambudio <pamela.loza199@gmail.com>
2018-05-27 20:41:05 +0200

    carlos

---
+++ app/src/main/java/fitness/sportgenertaion/fitnesstrainer/FuturasActualizaciones/AnyadirEjercicio.java
@@ -0,0 +1,15 @@
+package fitness.sportgenertaion.fitnesstrainer.FuturasActualizaciones;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import fitness.sportgenertaion.fitnesstrainer.R;
+
+public class AnyadirEjercicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anyadir_ejercicio);
+    }
+}
